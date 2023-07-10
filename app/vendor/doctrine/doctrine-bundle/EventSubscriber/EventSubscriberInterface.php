1d50994ec9774952a9b39171d024788fd116d6fa
Norivo7 <szymik.kamil97@gmail.com>
2023-07-10 11:17:29 +0200

    up project

---
+++ app/vendor/doctrine/doctrine-bundle/EventSubscriber/EventSubscriberInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Doctrine\Bundle\DoctrineBundle\EventSubscriber;
+
+use Doctrine\Common\EventSubscriber;
+
+/**
+ * @deprecated use the {@see AsDoctrineListener} attribute instead
+ */
+interface EventSubscriberInterface extends EventSubscriber
+{
+}
