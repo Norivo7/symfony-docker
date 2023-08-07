023be07c904c3571dc71a55387bb4e8b917a5fb9
Norivo7 <szymik.kamil97@gmail.com>
2023-08-07 10:50:44 +0200

    add testing controllers + install dev packages + fighting with xdebug config

--- app/src/Controller/TestController.php
+++ app/src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
@@ -15,5 +16,13 @@ class TestController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TestController.php',
         ]);
+    }
+
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        $test = "cos";
+        $test = $test . 'cos 2';
+        return $this->render('base.html.twig');
     }
 }
